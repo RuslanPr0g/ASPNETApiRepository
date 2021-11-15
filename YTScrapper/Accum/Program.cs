9f54110bf3ba4310372a0aafb03afd2a6c6716bb
Ruslan Rudenko <theruslan.prog@gmail.com>
2021-11-15 18:47:25 +0200

    Add accumulation project

---
+++ YTScrapper/Accum/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+
+namespace Accum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Accumulation("fDSGdg"));
+        }
+
+        private static string Accumulation(string text)
+        {
+            if (string.IsNullOrWhiteSpace(text))
+            {
+                return text;
+            }
+
+            StringBuilder stringBuilder = new();
+
+            var i = 0;
+            text.ToList().ForEach((c) =>
+            {
+                stringBuilder.Append(new string(c, i + 1));
+
+                if (i < text.Length - 1)
+                    stringBuilder.Append('-');
+
+                i++;
+            });
+
+            return stringBuilder.ToString().ToTitleCase();
+        }
+    }
+
+    static class Extentions
+    {
+        public static string ToTitleCase(this string s) =>
+            CultureInfo.InvariantCulture.TextInfo.ToTitleCase(s.ToLower());
+    }
+}
