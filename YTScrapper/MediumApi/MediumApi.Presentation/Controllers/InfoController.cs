c25350d3421bcf8f02ab8d90250cd05ab05756b8
Ruslan Rudenko <theruslan.prog@gmail.com>
2021-11-17 12:14:38 +0200

    Add global using and info controller

---
+++ YTScrapper/MediumApi/MediumApi.Presentation/Controllers/InfoController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MediumApi.Presentation.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InfoController : ControllerBase
+    {
+        [HttpGet("post")]
+        public async Task<IActionResult> Get()
+        {
+
+            return Ok();
+        }
+    }
+}
