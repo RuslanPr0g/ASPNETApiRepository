bf855d256342677fa13f012991a016fd7805eadc
Ruslan Rudenko <theruslan.prog@gmail.com>
2021-12-21 17:08:22 +0200

    UniqueInOrder

---
+++ YTScrapper/UniqueInOrder/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+
+var res = UniqueInOrder("AAAABBBCCDAABBB");
+
+Console.WriteLine();
+foreach (var item in res)
+{
+    Console.WriteLine(item);
+}
+Console.WriteLine();
+
+static IEnumerable<T> UniqueInOrder<T>(IEnumerable<T> input)
+{
+    var prev = default(T);
+
+    foreach (var elem in input)
+    {
+        if (prev is not null && prev.Equals(elem))
+            continue;
+
+        prev = elem;
+
+        yield return elem;
+    }
+}
